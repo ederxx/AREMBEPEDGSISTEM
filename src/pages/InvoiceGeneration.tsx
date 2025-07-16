f87767b85f829eefe63c183a782de102715c8167
ederxx <93275824+ederxx@users.noreply.github.com>
2025-07-16 15:59:30 -0300

    Update InvoiceGeneration.tsx

--- src/pages/InvoiceGeneration.tsx
+++ src/pages/InvoiceGeneration.tsx
@@ -58,7 +58,7 @@ queryFn: async () => {
   const snapshot = await getDocs(collection(db, 'services'));
   return snapshot.docs
     .map(doc => ({ id: doc.id, ...doc.data() as any }))
-    .filter(service => service.status === 'finalizado');
+    .filter(service => service.status === 'faturados');
 },
     enabled: !!user
   });
